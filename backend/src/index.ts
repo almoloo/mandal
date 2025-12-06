69641d7dd0e8e712bd40e32f9ed7db358b745f9c
Ali Mousavi <amousavig@icloud.com>
2025-12-06 15:49:17 +0330

    Added cors.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import contracts from './routes/contracts.js';
 import dapps from './routes/dapps.js';
 import reviews from './routes/reviews.js';
 
 const app = new Hono();
+
+app.use(
+	'*',
+	cors({
+		origin: `extension://${process.env.EXTENSION_ID}`,
+	})
+);
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!');
