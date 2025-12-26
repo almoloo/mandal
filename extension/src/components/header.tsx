cb28cd9ece0da920b92c597efd5e606c3053f7d9
Ali Mousavi <amousavig@icloud.com>
2025-12-27 02:16:41 +0330

    Added icons and github url.

--- extension/src/components/header.tsx
+++ extension/src/components/header.tsx
@@ -1,3 +1,17 @@
+import { Button } from '@/components/ui/base/buttons/button';
+import { CodeCircle03 } from '@untitledui/icons';
+
 export default function Header() {
-	return <header className="p-3 text-xl font-bold">mandal</header>;
+	return (
+		<header className="p-3 flex items-center justify-between">
+			<h1 className="text-xl font-bold">mandal</h1>
+			<Button
+				href="https://github.com/almoloo"
+				title="GitHub"
+				color="link-gray"
+			>
+				<CodeCircle03 className="w-5 h-5" />
+			</Button>
+		</header>
+	);
 }
